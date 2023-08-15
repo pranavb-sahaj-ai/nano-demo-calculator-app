63dc442208cfec236d8cdd99bd00b0a87604a622
pr <rvpspran@gmail.com>
2023-08-15 12:11:02 +0530

    Smallchanges

--- server.py
+++ server.py
@@ -9,18 +9,15 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    # take inputs from body
     body = request.get_json()
     sum=body['first']+body['second']
-    #return json response
     return {"result":sum}
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     body = request.get_json()
-    sum=body['first']-body['second']
-    #return json response
-    return {"result":sum}
+    diff=body['first']-body['second']
+    return {"result":diff}
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
