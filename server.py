b5f0dc09d1155ca02a97d79d9b216f3d01c0ee4e
Anish Ayyagari <83899953+flaminghawk1207@users.noreply.github.com>
2023-08-15 18:04:05 +0530

    solution

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 
@@ -9,12 +10,12 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     res=request.get_json()
-    return jsonify(int(res['first']+res['second']))
+    return jsonify(({'result': int(res['first']+res['second'])}))
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     res=request.get_json()
-    return jsonify(int(res['first']-res['second']))
+    return jsonify(({'result': int(res['first']-res['second'])}))
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
