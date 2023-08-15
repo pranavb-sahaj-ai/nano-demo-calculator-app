3794ecb66434faa6694c321009f9d8f0270a4b1a
Anish Ayyagari <83899953+flaminghawk1207@users.noreply.github.com>
2023-08-15 17:49:09 +0530

    trial 2

--- server.py
+++ server.py
@@ -10,12 +10,12 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     res=request.get_json()
-    return {"result":res['first']+res['second']}
+    return jsonify(Result(res['first']+res['second']))
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     res=request.get_json()
-    return {"result":res['first']-res['second']}
+    return jsonify(Result(res['first']-res['second']))
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
